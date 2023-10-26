8dc546d72d24d25fca7c969e2c50439a975fbb24
alph56 <111250517+alph56@users.noreply.github.com>
2023-10-26 04:46:05 +0000

    Tabla "mascota" realizada

--- mascota.sql
+++ mascota.sql
@@ -68,3 +68,13 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+CREATE TABLE `mascota`(
+  `caract-fisicas` varchar(255) NOT NULL,
+  `status-salud` varchar(255) NOT NULL,
+  `edad` varchar(5) NOT NULL,
+  `raza` varchar(50) NOT NULL,
+  `id-dueno` int (11) NOT NULL,
+  `fotografia` text NOT NULL,
+  foreign KEY(id-dueno) REFERENCES usuario(id) 
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
