a19313df94990b11b165443cadd9a797ad2a61bf
samnette <148294714+samnette@users.noreply.github.com>
2023-11-19 04:16:38 -0600

    SQL Messages

--- dogeway.sql
+++ dogeway.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-11-2023 a las 23:15:49
+-- Tiempo de generación: 19-11-2023 a las 11:15:36
 -- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+-- Versión de PHP: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -70,6 +70,27 @@ CREATE TABLE `match` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `messages`
+--
+
+CREATE TABLE `messages` (
+  `msg_id` int(11) NOT NULL,
+  `incoming_msg_id` int(255) NOT NULL,
+  `outgoing_msg_id` int(255) NOT NULL,
+  `msg` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `messages`
+--
+
+INSERT INTO `messages` (`msg_id`, `incoming_msg_id`, `outgoing_msg_id`, `msg`) VALUES
+(26, 987654321, 123456789, 'Message example 01'),
+(27, 987654321, 123456789, '%%$$Mesa!$\"&($Example);');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuario`
 --
 
@@ -90,20 +111,22 @@ CREATE TABLE `usuario` (
   `archivo_n` varchar(128) NOT NULL,
   `ine` text NOT NULL,
   `admin` int(2) NOT NULL DEFAULT 0,
-  `status` varchar(20) NOT NULL
+  `status` varchar(20) NOT NULL,
+  `unique_id` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`id`, `nombre`, `email`, `codigo_verificacion`, `verificado`, `pass`, `curp`, `telefono`, `nickname`, `apellidos`, `municipio`, `fechanac`, `fotografia`, `archivo_n`, `ine`, `admin`, `status`) VALUES
-(50, 'Daniel', 'esequien333@gmail.com', '6e5400def35b38c2a91f18dab7b0e7e9', 1, '202cb962ac59075b964b07152d234b70', 'JUSD030212HCLRLNA4', '8715243308', 'DanCatFly', 'Juarez', 'Guadalajara', '2003-02-12', 'f5931efe62aee7519a49676cb36ece32.jpg', 'communityIcon_y44umx9mwy041.jpg', 'JRSLDN03021205H600', 0, 'Disponible'),
-(51, 'Samantha', 'androlausam2@gmail.com', '9f77720ef7b5122aabf45b49dd1266c6', 1, '0cdfb256110772e88c21ddf94ee6f1ba', 'TEGL031023MJCLRRA1', '3310766433', 'samnette', 'Telles', 'Guadalajara', '2003-10-23', 'd06f547c157893df34569aa4ca374099.jpg', 'miyamurita.jpg', 'LSEGNM03021205M700', 1, 'Offline'),
-(52, 'Logan', 'alphc056@gmail.com', 'a616abf9d883b7ec31b4a6f259685ed4', 0, '622be4f47f555a3f8fc87d1d7cf31430', 'LXCA030811MJCPNLA0', '3324303048', 'alph56', 'Cuenca', 'Zapopan', '2003-08-11', 'ed7bbcb87ac9dbf04e3988a73bf81e17.png', 'Alphoto.png', 'LPCNAL03081114M000', 1, ''),
-(53, 'Christian', 'cristiansillochavech@gmail.com', '97fda7f4e1bb04c48b022eb09f3d998b', 0, '827ccb0eea8a706c4c34a16891f84e7b', 'FFFF030404HJCVFFF8', '3334945085', 'Cris Ruvalcaba', 'Ruvalcava', 'Guadalajara', '2003-04-04', '47d9e075126125bd9f6b97850df044c6.jpg', 'crisfoto.jpg', 'AAAAAA03040414H400', 1, ''),
-(54, 'Trilce', 'trilceflores15@gmail.com', 'c466a0eae55f4e1042bab05e1d49e5d0', 0, '637460cd3f487aa97158cfcc01a2f6b4', 'FLARTR03022114M201', '3318296199', 'trilce', 'Flores', 'Guadalajara', '2003-02-21', '60a0d0ccbae677e42adc5592f771a1a2.jpg', 'trilcefoto.jpg', 'TTTTTT21020314M120', 1, ''),
-(55, 'Diego', 'diegoalejandrolopezcoronado@hotmail.com', '50e8d5e64f0c3f7c743e63489569c78e', 0, '49aadac47c39944fd6078ac36e7e53bc', 'LOCD770814MJRPCGO0', '3357845617', 'diego', 'Lopez', 'Guadalajara', '1999-08-14', 'e41253bb71e52c8f14a518a519c84ef3.jpeg', 'diegofoto.jpeg', 'DDDDDD08119914H810', 1, '');
+INSERT INTO `usuario` (`id`, `nombre`, `email`, `codigo_verificacion`, `verificado`, `pass`, `curp`, `telefono`, `nickname`, `apellidos`, `municipio`, `fechanac`, `fotografia`, `archivo_n`, `ine`, `admin`, `status`, `unique_id`) VALUES
+(50, 'Daniel Antonio', 'esequien333@gmail.com', '6e5400def35b38c2a91f18dab7b0e7e9', 1, '202cb962ac59075b964b07152d234b70', 'JUSD030212HCLRLNA4', '8715243308', 'DanCatFly', 'Juarez Silva', 'Guadalajara', '2003-02-12', 'f5931efe62aee7519a49676cb36ece32.jpg', 'communityIcon_y44umx9mwy041.jpg', 'JRSLDN03021205H600', 0, 'Offline', 123456789),
+(51, 'Laura Samantha', 'androlausam2@gmail.com', '9f77720ef7b5122aabf45b49dd1266c6', 1, '0cdfb256110772e88c21ddf94ee6f1ba', 'TEGL031023MJCLRRA1', '3310766433', 'samnette', 'Telles Gordillo', 'Guadalajara', '2003-10-23', 'd06f547c157893df34569aa4ca374099.jpg', 'miyamurita.jpg', 'LSEGNM03021205M700', 0, 'Offline', 987654321),
+(52, 'Alberto', 'dangatovolador@gmail.com', 'e51fadadb39f8d188f495ab3a663c924', 1, 'caf1a3dfb505ffed0d024130f58c5cfa', 'JSTL031023MJCLRRS1', '4671007191', 'Juarez9516', 'Juárez Silva', 'Guadalajara', '2006-01-18', '05abde1caafd58db602eada746fb7404.jpg', 'soyese.jpg', 'AJEGNM03021205M711', 0, 'Offline', 793683764),
+(53, 'Christian', 'cristiansillochavech@gmail.com', '97fda7f4e1bb04c48b022eb09f3d998b', 0, '827ccb0eea8a706c4c34a16891f84e7b', 'FFFF030404HJCVFFF8', '3334945085', 'Cris Ruvalcaba', 'Ruvalcava', 'Guadalajara', '2003-04-04', '47d9e075126125bd9f6b97850df044c6.jpg', 'crisfoto.jpg', 'AAAAAA03040414H400', 1, 'Offline', 549412433),
+(54, 'Trilce', 'trilceflores15@gmail.com', 'c466a0eae55f4e1042bab05e1d49e5d0', 0, '637460cd3f487aa97158cfcc01a2f6b4', 'FLARTR03022114M201', '3318296199', 'trilce', 'Flores', 'Guadalajara', '2003-02-21', '60a0d0ccbae677e42adc5592f771a1a2.jpg', 'trilcefoto.jpg', 'TTTTTT21020314M120', 1, 'Offline', 586934568),
+(55, 'Diego', 'diegoalejandrolopezcoronado@hotmail.com', '50e8d5e64f0c3f7c743e63489569c78e', 0, '49aadac47c39944fd6078ac36e7e53bc', 'LOCD770814MJRPCGO0', '3357845617', 'diego', 'Lopez', 'Guadalajara', '1999-08-14', 'e41253bb71e52c8f14a518a519c84ef3.jpeg', 'diegofoto.jpeg', 'DDDDDD08119914H810', 1, 'Offline', 785432076),
+(56, 'Logan', 'alphc056@gmail.com', 'a616abf9d883b7ec31b4a6f259685ed4', 0, '622be4f47f555a3f8fc87d1d7cf31430', 'LXCA030811MJCPNLA0', '3324303048', 'alph56', 'Cuenca', 'Zapopan', '2003-08-11', 'ed7bbcb87ac9dbf04e3988a73bf81e17.png', 'Alphoto.png', 'LPCNAL03081114M000', 1, 'Offline', 639721044);
 
 --
 -- Índices para tablas volcadas
@@ -122,6 +145,12 @@ ALTER TABLE `mascota`
 ALTER TABLE `match`
   ADD PRIMARY KEY (`idmatch`),
   ADD KEY `fk_id1_usuario` (`id1`);
+
+--
+-- Indices de la tabla `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`msg_id`);
 
 --
 -- Indices de la tabla `usuario`
@@ -146,10 +175,16 @@ ALTER TABLE `match`
   MODIFY `idmatch` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=183;
 
 --
+-- AUTO_INCREMENT de la tabla `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `msg_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- Restricciones para tablas volcadas
