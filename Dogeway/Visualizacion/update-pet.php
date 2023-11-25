88e274c6f547e8c64f58371f8aa46ba7ae319ec5
alph56 <111250517+alph56@users.noreply.github.com>
2023-11-24 19:15:06 -0600

    Actualización de mascotas

---
+++ Dogeway/Visualizacion/update-pet.php
@@ -0,0 +1,39 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    require 'db.php';
+    $conexion = conectar();
+
+    if ($conexion->connect_error) {
+        die("Conexión fallida: " . $conexion->connect_error);
+    }
+    
+    $newUserId = $_POST['id_usuario'];
+    $newId = $_POST['id'];
+    $newNombreMasc = $_POST['nombreMascota'];
+    $newDesc = $_POST['descripcion'];
+    $newEspecie = $_POST['especie'];
+    $newRaza = $_POST['raza'];
+    $newEdad = $_POST['edad'];
+    $newEspecificacion = $_POST['especificaciones'];
+    $newCaract = $_POST['caracteristicas'];
+    $newCategoria = $_POST['categoria'];
+
+    $actualizar="UPDATE mascota SET nombreMascota='$newNombreMasc', descripcion='$newDesc', 
+    especie='$newEspecie', raza='$newRaza', edad='$newEdad', especificaciones='$newEspecificacion',
+    caracteristicas='$newCaract', categoria='$newCategorias'
+    WHERE id= '$newId' ";
+
+    // Ejecutar la consulta
+    if ($conexion->query($actualizar) === TRUE) {
+        header("Location: update-successfully.php");
+    } else {
+        echo "Error al actualizar: " . $conexion->error;
+    }
+    
+    // Cerrar la conexión
+    $conexion->close();
+
+}
+?>
